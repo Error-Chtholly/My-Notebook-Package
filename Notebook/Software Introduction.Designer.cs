3b8979cf8062a7d97b8f7bb14caafd155d4426e2
Error0227 <1696304992@qq.com>
2024-03-17 20:25:01 +0800

    添加文件

---
+++ Notebook/Software Introduction.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Notebook
+{
+    partial class Software_Introduction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Software_Introduction));
+            this.label1 = new System.Windows.Forms.Label();
+            this.softwareintroduction = new System.Windows.Forms.RichTextBox();
+            this.funtionintroduction = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Thistle;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(803, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "产品介绍(Software Introduction)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // softwareintroduction
+            // 
+            this.softwareintroduction.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.softwareintroduction.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.softwareintroduction.Font = new System.Drawing.Font("幼圆", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.softwareintroduction.Location = new System.Drawing.Point(12, 45);
+            this.softwareintroduction.Name = "softwareintroduction";
+            this.softwareintroduction.ReadOnly = true;
+            this.softwareintroduction.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.softwareintroduction.Size = new System.Drawing.Size(779, 191);
+            this.softwareintroduction.TabIndex = 1;
+            this.softwareintroduction.Text = resources.GetString("softwareintroduction.Text");
+            // 
+            // funtionintroduction
+            // 
+            this.funtionintroduction.BackColor = System.Drawing.Color.Thistle;
+            this.funtionintroduction.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.funtionintroduction.Location = new System.Drawing.Point(0, 239);
+            this.funtionintroduction.Name = "funtionintroduction";
+            this.funtionintroduction.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.funtionintroduction.Size = new System.Drawing.Size(803, 42);
+            this.funtionintroduction.TabIndex = 2;
+            this.funtionintroduction.Text = "功能简介(Function Introduction)";
+            this.funtionintroduction.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("幼圆", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 284);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.richTextBox1.Size = new System.Drawing.Size(779, 308);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Thistle;
+            this.label2.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 595);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(803, 42);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "版本简介(Version Introduction)";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Font = new System.Drawing.Font("幼圆", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox2.Location = new System.Drawing.Point(7, 640);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.richTextBox2.Size = new System.Drawing.Size(779, 115);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "应用名称：My Notebook            开发者：Error\n应用版本：v 1.2.6                开发语言：C#(C Sharp" +
+    ")\n开发时间：2024.03.17             默认支持操作系统：Win10及以上\n版本更新：见软件更新日志         所需系统环境：.NET" +
+    " Freamwork 4.8";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(3, 748);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(800, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "_________________________________________________________________________________" +
+    "_______";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Thistle;
+            this.button1.Font = new System.Drawing.Font("幼圆", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(675, 780);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 41);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "确  定";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Software_Introduction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(803, 834);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.funtionintroduction);
+            this.Controls.Add(this.softwareintroduction);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Software_Introduction";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Software Introduction";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox softwareintroduction;
+        private System.Windows.Forms.Label funtionintroduction;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
